93c8718d465975fb2767232b6fee0b2868358b1b
Antonio Falletta <antoniofalletta2000@gmail.com>
2026-04-21 16:23:05 +0200

    fix navbar

--- components/AppHeader.jsx
+++ components/AppHeader.jsx
@@ -2,12 +2,9 @@ export default function AppHeader() {
     return (
         <>
             <nav className="navbar bg-body-tertiary">
-                <div className="container-fluid ">
-                    <div className=" container d-flex justify-content-between ">
-                        <a className="navbar_brand text-decoration-none text-black" href="/movie">HomePage</a>
-                        <a className="text-decoration-none text-black" >About</a>
-                    </div>
-
+                <div className="container">
+                    <a className="text-decoration-none text-black" href="/movie">HomePage</a>
+                    <a className="text-decoration-none text-black">About</a>
                 </div>
             </nav>
         </>
