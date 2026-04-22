dd411c527b6ba39a3abfcac9c8917cc471f004df
Antonio Falletta <antoniofalletta2000@gmail.com>
2026-04-22 13:04:04 +0200

    change hover effect in card and onClick subject

--- components/MoviesMap.jsx
+++ components/MoviesMap.jsx
@@ -13,35 +13,38 @@ export default function MoviesMap({ movie }) {
     }
 
     return (
-        <div className="col">
-            <div id="movies_card" className="card text-white bg-black">
+        <div className="col p-3">
+            <div id="movies_card" onClick={toggleDetails} className="card text-white bg-black">
 
-                {details?
-                <div className="d-flex justify-content-center ">
-                    <i id="icon_eye" className="bi bi-eye-slash text-white"></i>
-                </div> : 
-                <div className="d-flex justify-content-center ">
-                    <i id="icon_eye" className="bi bi-eye text-white"></i>
-                </div>}
+                {details ?
+                    <div className="d-flex justify-content-center ">
+                        <i id="icon_eye" className="bi bi-eye-slash text-white"></i>
+                    </div> :
+                    <div className="d-flex justify-content-center ">
+                        <i id="icon_eye" className="bi bi-eye text-white"></i>
+                    </div>}
 
                 <h4 className="card-title d-flex justify-content-center pt-5">{movie.title}</h4>
                 <div className="card-body">
-                    <img onClick={toggleDetails} id="movies_image" src={`${url}/${movie.image}`} alt={movie.title} className="img-fluid" />
-                    
-                    {details && <>
-                        <p className="card-text pt-2">
-                            <p className="fw-bold">Abstract: </p>
-                            {movie.abstract}
-                        </p>
-                        <p className="d-flex justify-content-end gap-2"><p className="fw-bold">Release Date:</p> {movie.release_year}</p>
-                        <div className="d-flex justify-content-end">
-                            <button className="btn border border-0 bg-primary">
-                                <Link className="text-decoration-none text-white" to={`/movie/${movie.id}`}>View Movie</Link>
-                            </button>
-                        </div>
-                    </>
+
+                    {details ?
+                        <>
+                            <p className="card-text pt-2">
+                                <p className="fw-bold">Abstract: </p>
+                                {movie.abstract}
+                            </p>
+                            <p className="d-flex justify-content-end gap-2"><p className="fw-bold">Release Date:</p> {movie.release_year}</p>
+                            <div className="d-flex justify-content-end">
+                                <button className="btn border border-0 bg-primary">
+                                    <Link className="text-decoration-none text-white" to={`/movie/${movie.id}`}>View Movie</Link>
+                                </button>
+                            </div>
+                        </>
+
+                        : <img id="movies_image" src={`${url}/${movie.image}`} alt={movie.title} className="img-fluid" />
+
                     }
-                    
+
                 </div>
             </div>
         </div>
