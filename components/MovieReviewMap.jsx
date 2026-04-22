9a3db5ae58d2dd0f5dad2ad01aac1c55f40a308b
Antonio Falletta <antoniofalletta2000@gmail.com>
2026-04-22 10:59:53 +0200

    add vote stars and new font

--- components/MovieReviewMap.jsx
+++ components/MovieReviewMap.jsx
@@ -1,5 +1,18 @@
 export default function SingleMovieMap({ review }) {
 
+    function stars({vote}){
+        const starsArr=[]
+        for(let i=0; i<=5; i++){
+            starsArr.push(
+                <i key={i} className={
+                    i<=vote ? "bi bi-star-fill text-warning" : "bi bi-star text-warning"
+                }></i>
+            )
+
+        }
+        return starsArr;
+    }
+
     return (
         <div className="border rounded p-3 mt-3 d-flex flex-column gap-4 bg-black" >
             <div className="bg-light">
@@ -7,7 +20,7 @@ export default function SingleMovieMap({ review }) {
             </div>
             <div className="text-white">
                 <p className="d-flex gap-3"><p className="fw-bold">Commento:</p>{review.text}</p>
-                <p className="d-flex gap-3"><p className="fw-bold">Voto:</p>{review.vote}</p>
+                <p className="d-flex gap-3"><p className="fw-bold">Voto:</p>{stars({ vote: review.vote })}</p>
             </div>
         </div>
     )
